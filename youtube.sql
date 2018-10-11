28f1a205893f81ef6d65b95325d445188919eded
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2018-10-11 09:17:51 +0200

    Nueva tabla comentario

--- youtube.sql
+++ youtube.sql
@@ -18,6 +18,38 @@ USE `youtube`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comentario`
+--
+
+DROP TABLE IF EXISTS `comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` datetime DEFAULT CURRENT_TIMESTAMP,
+  `texto` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `aprobado` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0=sin aprobar\n1=aprobado',
+  `id_video` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comentario_has_usuario_idx` (`id_usuario`),
+  KEY `comentario_has_cideo_idx` (`id_video`),
+  CONSTRAINT `comentario_has_cideo` FOREIGN KEY (`id_video`) REFERENCES `video` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comentario_has_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentario`
+--
+
+LOCK TABLES `comentario` WRITE;
+/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+INSERT INTO `comentario` VALUES (1,'2018-10-11 09:10:25','Me ha encantado el video.',0,1,2),(2,'2018-10-11 09:11:33','No me ha gustado nada este video.',0,1,3);
+/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `rol`
 --
 
@@ -28,7 +60,7 @@ CREATE TABLE `rol` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +69,7 @@ CREATE TABLE `rol` (
 
 LOCK TABLES `rol` WRITE;
 /*!40000 ALTER TABLE `rol` DISABLE KEYS */;
-INSERT INTO `rol` VALUES (1,'Administrador'),(2,'Usuario normal'),(3,'Usuario limitado'),(4,'Usuario con inserccion'),(5,'colaborador');
+INSERT INTO `rol` VALUES (1,'Administrador'),(2,'Usuario normal'),(3,'Usuario limitado'),(4,'Usuario con inserccion'),(5,'colaborador'),(6,'jefe');
 /*!40000 ALTER TABLE `rol` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +89,7 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `nombre_UNIQUE` (`nombre`),
   KEY `usuario_has_rol_idx` (`id_rol`),
   CONSTRAINT `usuario_has_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +98,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'admin','admin',1),(16,'manolito','12345678',2),(26,'pepeelmas','pepeelmas',2),(29,'pepe','admin',2),(30,'soledadlapesada','admin',2),(32,'ainara','admin',2),(33,'adminlo','admin',2),(36,'pepito','grilllo',2),(37,'ainarag','ainarag',2);
+INSERT INTO `usuario` VALUES (1,'admin','admin',1),(2,'manolo','12345',2),(3,'pepi','12345',2),(30,'soledadla','admin',2),(32,'ainara','admin',2),(33,'adminlo','admin',2),(36,'pepito','grilllo',2),(37,'ainarag','ainarag',2),(47,'ainaragoitia','ainara',1),(48,'manuel','ainara',1),(51,'Metallica','metalica',5);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +118,7 @@ CREATE TABLE `video` (
   UNIQUE KEY `codigo_UNIQUE` (`codigo`),
   KEY `video_has_usuario_idx` (`id_usuario`),
   CONSTRAINT `video_has_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +127,7 @@ CREATE TABLE `video` (
 
 LOCK TABLES `video` WRITE;
 /*!40000 ALTER TABLE `video` DISABLE KEYS */;
-INSERT INTO `video` VALUES (1,'6_ojUdgLXj0','Sôber & Orquesta O.C.A.S. - Arrepentido',1),(2,'LOdbSsZ0Mn4','SAUROM - Se Acerca El Invierno (Juego de Tronos - con Julia Medina)',1),(3,'wL2012Q3h3M','Y serás canción - Big Simon',1),(4,'tAGnKpE4NCI','Metallica - Nothing Else Matters [Official Music Video]',1),(8,'qxZInIyOBXk','Pat Benatar - We Belong',1),(10,'lDK9QqIzhwk','Bon Jovi - Livin\' On A Prayer',1),(14,'vx2u5uUu3DE','Bon Jovi - It\'s My Life',1);
+INSERT INTO `video` VALUES (1,'6_ojUdgLXj0','Sôber & Orquesta O.C.A.S. - Arrepentido',1),(2,'LOdbSsZ0Mn4','SAUROM - Se Acerca El Invierno (Juego de Tronos - con Julia Medina)',1),(3,'wL2012Q3h3M','Y serás canción - Big Simon',1),(4,'tAGnKpE4NCI','Metallica - Nothing Else Matters [Official Music Video]',1),(8,'qxZInIyOBXk','Pat Benatar - We Belong',1),(10,'lDK9QqIzhwk','Bon Jovi - Livin\' On A Prayer',1),(14,'vx2u5uUu3DE','Bon Jovi - It\'s My Life',1),(24,'JV8-fFpMnsk','Mago de Oz - La costa del silencio',32);
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -108,4 +140,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-09 10:45:56
+-- Dump completed on 2018-10-11  9:16:24
