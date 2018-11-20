6c4dfcc045a0885afc550d582bc5d85937b85476
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2018-11-20 11:41:37 +0100

    Cambio alert de package

--- src/main/java/com/ipartek/formacion/youtube/controller/back/BackofficeComentarioController.java
+++ src/main/java/com/ipartek/formacion/youtube/controller/back/BackofficeComentarioController.java
@@ -11,10 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ipartek.formacion.youtube.controller.pojo.Alert;
 import com.ipartek.formacion.youtube.model.ComentarioDAO;
 import com.ipartek.formacion.youtube.model.UsuarioDAO;
 import com.ipartek.formacion.youtube.model.VideoDAO;
-import com.ipartek.formacion.youtube.pojo.Alert;
 import com.ipartek.formacion.youtube.pojo.Comentario;
 
 /**
